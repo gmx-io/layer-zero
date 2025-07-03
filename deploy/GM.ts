413578093050df075ebbcf6e1acfd4436e896093
shankar <shankar@layerzerolabs.org>
2025-07-03 16:07:00 +0000

    project init
    Signed-off-by: shankar <shankar@layerzerolabs.org>

---
+++ deploy/GM.ts
@@ -0,0 +1,65 @@
+import assert from 'assert'
+
+import { type DeployFunction, DeployResult } from 'hardhat-deploy/types'
+
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+const contractName = 'GM'
+
+const deploy: DeployFunction = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    assert(deployer, 'Missing named deployer account')
+
+    console.log(`Network: ${hre.network.name}`)
+    console.log(`Deployer: ${deployer}`)
+
+    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+
+    const eid = hre.network.config.eid
+    const networkName = hre.network.name
+
+    let oftDeployment: DeployResult
+
+    if (eid === EndpointId.SEPOLIA_V2_TESTNET || eid === EndpointId.ETHEREUM_V2_MAINNET) {
+        console.log(`Deploying ${contractName} lockbox adapter on ${networkName}`)
+        const gmTokenAddress = hre.network.config.oftAdapter?.gmTokenAddress
+
+        if (!gmTokenAddress) {
+            console.warn(`oftAdapter not configured on network config, skipping GM Adapter deployment`)
+            return
+        }
+
+        oftDeployment = await deploy(contractName, {
+            from: deployer,
+            args: [
+                gmTokenAddress, // token address
+                endpointV2Deployment.address, // LayerZero's EndpointV2 address
+                deployer, // owner
+            ],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        })
+    } else {
+        console.log(`Deploying ${contractName} OFT`)
+
+        oftDeployment = await deploy(contractName, {
+            from: deployer,
+            args: [
+                endpointV2Deployment.address, // LayerZero's EndpointV2 address
+                deployer, // owner
+            ],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        })
+    }
+
+    console.log(`Deployed contract: ${contractName}, network: ${networkName}, address: ${oftDeployment.address}`)
+}
+
+deploy.tags = [contractName]
+
+export default deploy
---
+++ type-extensions.ts
@@ -0,0 +1,24 @@
+import 'hardhat/types/config'
+
+interface OftAdapterConfig {
+    gmTokenAddress: string
+    glvTokenAddress: string
+}
+
+declare module 'hardhat/types/config' {
+    interface HardhatNetworkUserConfig {
+        oftAdapter?: never
+    }
+
+    interface HardhatNetworkConfig {
+        oftAdapter?: never
+    }
+
+    interface HttpNetworkUserConfig {
+        oftAdapter?: OftAdapterConfig
+    }
+
+    interface HttpNetworkConfig {
+        oftAdapter?: OftAdapterConfig
+    }
+}

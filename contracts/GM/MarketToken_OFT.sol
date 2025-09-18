d6ba574d270b259da52d10572b2c67dcbf5616d5
shankar <shankar@layerzerolabs.org>
2025-09-18 03:55:50 +0000

    chore: add openzeppelin permit and use msg.sender to avoid via-ir
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- contracts/GM/MarketToken_OFT.sol
+++ contracts/GM/MarketToken_OFT.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.22;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
-contract MarketToken_OFT is OFT {
+contract MarketToken_OFT is OFT, ERC20Permit {
+    /// @dev Using msg.sender instead of _delegate in Ownable to avoid via-ir
     constructor(
         string memory _name,
         string memory _symbol,
         address _lzEndpoint,
         address _delegate
-    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) ERC20Permit(_name) Ownable(msg.sender) {}
 }

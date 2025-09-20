e2d17a7469d59c23d95e2bc97611698df87243e1
shankar <shankar@layerzerolabs.org>
2025-09-20 14:23:49 +0000

    chore: split to gm and glv
    Signed-off-by: shankar <shankar@layerzerolabs.org>

--- layerzero.glv.testnet.config.ts
+++ layerzero.glv.testnet.config.ts
@@ -2,13 +2,12 @@ import { DVNConfigs, generateWireConfig } from './devtools'
 
 export default async function () {
     // Generate wire configs for both token types
-    /// Using default testnet dvns
+    /// Using default mainnet dvns
     const glvWireConfig = await generateWireConfig('GlvToken', DVNConfigs.testnet)
-    const gmWireConfig = await generateWireConfig('MarketToken', DVNConfigs.testnet)
 
     // Combine contracts and connections
     return {
-        contracts: [...glvWireConfig.contracts, ...gmWireConfig.contracts],
-        connections: [...glvWireConfig.connections, ...gmWireConfig.connections],
+        contracts: glvWireConfig.contracts,
+        connections: glvWireConfig.connections,
     }
 }
